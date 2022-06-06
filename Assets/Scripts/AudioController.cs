f044377f4fb2bd126c0f9337e3bf8f9e46fe409d
ariakm25 <ariakm25@gmail.com>
2022-06-06 22:38:30 +0700

    Init Main Menu

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	public void Mute()
+	{
+		AudioListener.pause = true;
+	}
+
+	public void UnMute()
+	{
+		AudioListener.pause = false;
+	}
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		// TODO - Load the game scene
+	}
+
+	public void LeaderBoard()
+	{
+		SceneManager.LoadScene("Leaderboard");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quitting Game");
+		Application.Quit();
+	}
+}
